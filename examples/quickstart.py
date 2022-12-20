2dc8afd371f789326699c1ff9d5fcc8aa4a7830f
James Chien <shc261392@gmail.com>
2022-12-20 05:40:20 +0000

    feat(quickstart.py): add quickstart script example

---
+++ examples/quickstart.py
@@ -0,0 +1,18 @@
+from plurk import Client
+
+APP_KEY = '<your-plurk-app-key>'
+APP_SECRET = '<your-plurk-app-secret>'
+
+
+with Client(APP_KEY, APP_SECRET) as client:
+    # Get app user's access token
+    request_token = client.get_request_token()
+    auth_url = client.get_auth_url(request_token)
+    print('Plurk OAuth authorization URL (open it with browser): ', auth_url)
+    auth_code = input('Please input the authorization code retrieved from authorization URL: ')
+    client.fetch_access_token(request_token, auth_code)
+
+    # Access Plurk API
+    user_data = client.users.me()
+    print('Display name: ', user_data.display_name)
+    print('Plurks created: ', user_data.plurks_count)
